2c0e8c6ab3b2a2e25b6e9639355e0a775e18b500
daniel <daniel.hung@lctech.com.tw>
2023-04-02 14:18:22 +0800

    test

--- db/db.go
+++ db/db.go
@@ -16,12 +16,12 @@ var db *sql.DB
 
 func InitDatabase(config utils.Config) (db *sql.DB, err error) {
 	dsn := fmt.Sprintf("%s://%s:%s@%v:%v/%s?sslmode=disable",
-				config.Connection,
-				config.Username,
-				config.Password,
-				config.Host,
-				config.Port,
-				config.Database,
+		config.Connection,
+		config.Username,
+		config.Password,
+		config.Host,
+		config.Port,
+		config.Database,
 	)
 
 	db, err = sql.Open(config.Connection, dsn)
@@ -55,22 +55,22 @@ func Close() {
 // }
 
 func initMigrate(db *sql.DB, database string) error {
-		driver, err := postgres.WithInstance(db, &postgres.Config{})
-		if err != nil {
-			return err
-		}
+	driver, err := postgres.WithInstance(db, &postgres.Config{})
+	if err != nil {
+		return err
+	}
 
-		m, err := migrate.NewWithDatabaseInstance(
-			"file://db/migrations",
-			database,
-			driver,
-		)
-		 // or m.Step(2) if you want to explicitly set the number of migrations to run
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://db/migrations",
+		database,
+		driver,
+	)
+	// or m.Step(2) if you want to explicitly set the number of migrations to run
 
-		if err := m.Up(); err != nil && err != migrate.ErrNoChange {
-			return err
-		}
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		return err
+	}
 
-		fmt.Println("---------  initializing migrations is successful ----")
-		return nil
+	fmt.Println("---------  initializing migrations is successful ----")
+	return nil
 }
